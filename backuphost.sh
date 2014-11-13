c85bcf3a3b1fe942fa510d901dda80d31ccd6051
Grzegorz Gliński <milyges@gmail.com>
2014-11-13 13:03:43 +0100

    Dodano mozliwosc backupowania wielu hostow jednym poleceniem

--- backuphost.sh
+++ backuphost.sh
@@ -2,7 +2,7 @@
 
 if [ -z "${1}" ]
 then
-	echo "Usage: $0 host"
+	echo "Usage: $0 host1 host2 ..."
 	exit 1
 fi
 
@@ -11,32 +11,38 @@ UBDIR="$(dirname -- ${0})"
 . "${UBDIR}/config.sh"
 . "${UBDIR}/functions.sh"
 
-IP="${1}"
-echo "Starting backup on host ${IP}" | log
-HOST="$(get_remote_hostname ${IP})"
-if [ -z "${HOST}" ]
-then
-	echo "Error getting remote host name, exiting..." | log
-	exit 2
-fi
-
-
-echo "Remote hostname is ${HOST}, getting config file." | log
-
-CONFIG="$(mktemp)"
-
-get_config_file "${IP}" > "${CONFIG}"
-
-echo "Rotating backup directory..." | log
-rotate_dir "$(get_host_dir ${HOST})" "${KEEP}" "$(get_backup_dir "${HOST}")"
-
-# Pobieramy liste zadan
-awk -F '.' '$1 == "job" { print $2 }' "${CONFIG}" | sort | uniq | while read JOB
+while [ -n "${1}" ]
 do
-	echo "Starting job ${JOB} on ${HOST}" | log
-	TYPE="$(grep "job.${JOB}.type" "${CONFIG}" | awk -F'=' '{ print $2 }' |  sed 's/^[ \t]*//;s/[ \t]*$//')"
-	backup_${TYPE}
+	IP="${1}"
+	echo "Starting backup on host ${IP}" | log
+	HOST="$(get_remote_hostname ${IP})"
+	if [ -z "${HOST}" ]
+	then
+		echo "Error getting remote host name, exiting..." | log
+		shift
+		continue
+	fi
+
+
+	echo "Remote hostname is ${HOST}, getting config file." | log
+
+	CONFIG="$(mktemp)"
+
+	get_config_file "${IP}" > "${CONFIG}"
+
+	echo "Rotating backup directory..." | log
+	rotate_dir "$(get_host_dir ${HOST})" "${KEEP}" "$(get_backup_dir "${HOST}")"
+
+	# Pobieramy liste zadan
+	awk -F '.' '$1 == "job" { print $2 }' "${CONFIG}" | sort | uniq | while read JOB
+	do
+		echo "Starting job ${JOB} on ${HOST}" | log
+		TYPE="$(grep "job.${JOB}.type" "${CONFIG}" | awk -F'=' '{ print $2 }' |  sed 's/^[ \t]*//;s/[ \t]*$//')"
+		backup_${TYPE}
+	done
+
+	rm -f "${CONFIG}"
+	echo "All jobs on ${HOST} completed" | log
+	shift
 done
 
-
-rm -f "${CONFIG}"
